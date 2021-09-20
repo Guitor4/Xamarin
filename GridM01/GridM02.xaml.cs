f1e3a73877a6703ff3786887a6a1bac1627c275d
GuiTor4 <guilhermetor10@gmail.com>
2021-09-20 11:55:30 -0300

    Adicionar arquivos de projeto.

---
+++ GridM01/GridM02.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GridM01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GridM02 : ContentPage
+    {
+        public GridM02()
+        {
+            InitializeComponent();
+        }
+
+        private void pagina01(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+        private void pagina03(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page03());
+        }
+    }
+}
---
+++ GridM01/Page03.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GridM01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page03 : ContentPage
+    {
+        public Page03()
+        {
+            InitializeComponent();
+        }
+        private void pagina02(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GridM02());
+        }
+        private void pagina01(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
